a2f1b395e4dc356785686ff64f4007a8b45d5dc4
Sudhir <deshwalsudhir07@gmail.com>
2020-05-28 09:51:17 -0400

    Final commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by user163862 on 5/28/20.
+//  Copyright © 2020 Sudhir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
